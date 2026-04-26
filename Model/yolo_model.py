e11e45ec243c45fcd7a35b5fdab70c9b358c09f7
Alanna <alannaccc@gmail.com>
2026-04-26 15:38:12 -0700

    yaml file and model.py update

--- Model/yolo_model.py
+++ Model/yolo_model.py
@@ -4,6 +4,21 @@ from ultralytics import YOLO
 model = YOLO("yolov3.pt")
 
 # https://docs.ultralytics.com/modes/train/#musgd-optimizer
-model.train(data = dataset.yaml)
+model.train(data = "dataset.yaml")
 
-results = model.predict()  
+print("Starting training...")
+model.train(data="dataset.yaml", 
+            epochs = 20, # cause small dataset sample
+            imgsz = 416, # not 400 cause of mulitple of 32??
+            val = True, # default
+            plots = True # default
+            )
+
+best_model = YOLO("runs/detect/train/weights/best.pt")
+
+# https://docs.ultralytics.com/modes/predict/#inference-sources
+results = best_model.predict(
+    source="path/to/test/images",  
+    save=True,                     # saves images with boxes
+    conf=0.25                      # confidence threshold (what is it?)
+)
